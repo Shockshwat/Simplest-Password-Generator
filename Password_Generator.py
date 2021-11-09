fa3bd391b851a0d77a4e7ded57f0f97c6b3501e9
Shockshwat <71540383+Shockshwat@users.noreply.github.com>
2021-11-09 22:48:56 +0530

    Add files via upload

---
+++ Password_Generator.py
@@ -0,0 +1,13 @@
+import random
+lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+upper_alphabet = lower_alphabet.upper()
+numbers = '1234567890'
+symbols = '!@#$%^&*)(_-=+";:.,?'
+all = list(lower_alphabet + upper_alphabet + numbers + symbols)
+random.shuffle(all)
+def password_generator(length):
+    password = []
+    for i in range(length):
+        password.append(random.choice(all))
+    random.shuffle(password)
+    print(''.join(password))
